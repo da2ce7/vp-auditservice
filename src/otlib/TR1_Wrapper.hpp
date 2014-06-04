aef015ed6c47400ffa960cb2a8644f84ebaa797f
yamamushi <yamamushi@gmail.com>
2014-06-03 21:21:00 -0500

    Ifdef Fixes

--- src/otlib/TR1_Wrapper.hpp
+++ src/otlib/TR1_Wrapper.hpp
@@ -10,23 +10,36 @@
 #endif
 
 #ifndef OT_USE_TR1
-#define _CINTTYPES <cinttypes>
+#ifdef __linux__
+#define _CINTTYPES <tr1/cinttypes>
+#define _MEMORY <tr1/memory>
+#include <boost/shared_ptr.hpp>
+#endif
+#ifdef __APPLE__
+#define _CINTTYPES <inttypes>
 #define _MEMORY <memory>
+#endif
 #else
 #define _CINTTYPES <tr1/cinttypes>
 #define _MEMORY <tr1/memory>
 #endif
 
 #ifndef OT_USE_TR1
+#ifdef __linux__
+#define _SharedPtr boost::shared_ptr
+#define _WeakPtr boost::weak_ptr
+#define _UniquePtr boost::unique_ptr
+#endif
+#ifdef __APPLE__
 #define _SharedPtr std::shared_ptr
 #define _WeakPtr std::weak_ptr
 #define _UniquePtr std::unique_ptr
+#endif
 #else
 #define _SharedPtr std::tr1::shared_ptr
 #define _WeakPtr std::tr1::weak_ptr
 #define _UniquePtr std::tr1::unique_ptr
 #endif
-
 
 #endif //__TR1_WRAPPER_HPP__
 
